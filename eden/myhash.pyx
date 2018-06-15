1d01140d352acc984176ebd62eb25536ae31cab7
Daniel Maticzka <maticzkd@informatik.uni-freiburg.de>
2018-06-15 16:06:38 +0200

    make work with python 2 and 3

--- eden/myhash.pyx
+++ eden/myhash.pyx
@@ -1,8 +1,9 @@
 cdef extern from 'myhash_impl.c':
     long newhash(unsigned char* c_string, Py_ssize_t c_len)
 
-def python_wrapper_for_myhash(bytes py_string):
-    # py_string = 'hellomydear'
-    cdef unsigned char* c_string = py_string
+def python_wrapper_for_myhash(py_string):
+    # required to convert to c_string from python 2 and 3
+    utf = py_string.encode("UTF-8")
+    cdef unsigned char* c_string = utf
     cdef Py_ssize_t c_len = len(py_string)
     return(newhash(c_string, c_len))
