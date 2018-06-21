dca2403a02289c90be37726e10d5b76f0c53269e
Daniel Maticzka <maticzkd@informatik.uni-freiburg.de>
2018-06-21 11:41:09 +0200

    final failed attempt to implement nicely

--- eden/hash.pyx
+++ eden/hash.pyx
@@ -1,15 +1,13 @@
 from cpython.ref cimport PyObject
 
 cdef extern from 'hash_impl.c':
-    long hash_bytes(unsigned char* c_string, Py_ssize_t c_len)
-    long hash_tuple(PyObject *v)
+    long hash_bytes(PyObject *po)
+    long hash_tuple(PyObject *po)
 
-def py2hash_bytes(bytes b):
-    cdef unsigned char* c_string = b
-    cdef Py_ssize_t c_len = len(b)
-    return(hash_bytes(c_string, c_len))
+def py2hash_bytes(b):
+    cdef PyObject* po = <PyObject*> b
+    return(hash_bytes(po))
 
 def py2hash_tuple(tuple t):
     cdef PyObject* po = <PyObject*> t
     return(hash_tuple(po))
-    return(0)
--- eden/hash_impl.c
+++ eden/hash_impl.c
@@ -2,19 +2,25 @@
 #include <ctype.h>
 #include <stddef.h>
 
-static long hash_bytes(unsigned char* p, Py_ssize_t len)
+static long hash_bytes(PyObject *o)
 {
+    PyBytesObject *a = (PyBytesObject*) o;
+
+    register Py_ssize_t len;
+    register unsigned char *p;
     register long x;
-    register Py_ssize_t i;
 
+    len = Py_SIZE(a);
+    if (len == 0) {
+        return 0;
+    }
+    p = (unsigned char *) a->ob_sval;
     x = 0;
-    i = len;
-
     x ^= *p << 7;
-    while (--i >= 0) {
+    while (--len >= 0) {
         x = (1000003*x) ^ *p++;
     }
-    x ^= len;
+    x ^= Py_SIZE(a);
 
     // avoid -1, denotes error in python hash function
     if (x == -1)
@@ -28,13 +34,14 @@ hash_tuple(PyObject *v)
 {
     PyTupleObject* to = (PyTupleObject*) v;
     register long x, y;
-    register Py_ssize_t len = Py_SIZE(v);
+    register Py_ssize_t len = Py_SIZE(to);
     register PyObject **p;
     long mult = 1000003L;
     x = 0x345678L;
     p = to->ob_item;
-    while (--len >= 0) {
-        y = PyObject_Hash(*p++);
+    while (--len >= 0)
+        PyObject* uc = (PyObject*) *p++;
+        y = hash_bytes(uc);
         if (y == -1)
             return -1;
         x = (x ^ y) * mult;
